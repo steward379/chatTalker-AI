06eaac64619b15c849f348528e7a0912a3b2e423
Murmurline <steward379@gmail.com>
2022-06-16 18:25:30 +0800

    jQuery QA
    jQuery QA

--- index.js
+++ index.js
@@ -39,25 +39,25 @@
 //     },
 // });    
 
-// const myswiper = new Swiper ('.swiper', {
-//     modules: [Navigation, Pagination],
-//     // direction: 'vertical', // 垂直切换选项
-//     loop: true, // 循环模式选项
+const myswiper = new Swiper ('.swiper', {
+    modules: [Navigation, Pagination],
+    // direction: 'vertical', // 垂直切换选项
+    loop: true, // 循环模式选项
     
-//     // 如果需要分页器
-//     pagination: {
-//       el: '.swiper-pagination',
-//       clickable: true,
-//     },
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
     
-//     // 如果需要前进后退按钮
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
     
-//     // 如果需要滚动条
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//     },
-// });
+    // 如果需要滚动条
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
